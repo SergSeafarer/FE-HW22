cd483fb07531a03691135b8860d9492d23265a3f
Sergii <seregaonma@gmail.com>
2023-11-12 22:19:48 +0200

    Add methods for topping and calculation price/calories

--- js/main.js
+++ js/main.js
@@ -44,4 +44,56 @@ class Hamburger {
     price: '15 tugriks',
     calories: 0,
   }
+
+  addTopping (additive) {
+    this.topping.push(additive);
+  }
+
+  calculatePrice() {
+    let {price: hamburgerPrice} = Hamburger;
+    const {price: sizePrice} = this.size;
+    const {price: stuffingPrice} = this.stuffing;
+    
+    if(this.topping.length === 0) {
+      hamburgerPrice = Number.parseInt(sizePrice) + Number.parseInt(stuffingPrice);
+      this.price = hamburgerPrice;
+    } 
+    if(this.topping.length > 0) {
+      const toppingPrice = this.topping.map(obj => obj.price).reduce((totalPrice, currentPrice) => Number.parseInt(totalPrice) + Number.parseInt(currentPrice),);
+      hamburgerPrice = Number.parseInt(sizePrice) + Number.parseInt(stuffingPrice) + Number.parseInt(toppingPrice);
+      this.price = hamburgerPrice;
+    }
+    return hamburgerPrice;
+  }
+
+  calculateCalories() {
+    let {calories: hamburgerCalories} = Hamburger;
+    const {calories: sizeCalories} = this.size;
+    const {calories: stuffingCalories} = this.stuffing;
+
+    if(this.topping.length === 0) {
+      hamburgerCalories = sizeCalories + stuffingCalories;
+      this.calories = hamburgerCalories; 
+    }
+    if(this.topping.length > 0) {
+      const toppingCalories = this.topping.map(obj => obj.calories).reduce((totalCalories, currentCalories) => totalCalories + currentCalories,);
+      hamburgerCalories = sizeCalories + stuffingCalories + toppingCalories;
+      this.calories = hamburgerCalories;
+    }
+    return hamburgerCalories;
+  }
 }
+
+const hamburger = new Hamburger(Hamburger.SIZE_BIG, Hamburger.STUFFING_CHEESE);
+console.log('Currently hamburger price without toppoing is: ' + hamburger.calculatePrice() + ' tugriks');
+console.log('Hamburger calories without topping: ' + hamburger.calculateCalories() + ' calories');
+
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+console.log('Hamburger price with mayonaisse is: ' + hamburger.calculatePrice() + ' tugriks');
+console.log('Hamburger calories with mayonnaise: ' + hamburger.calculateCalories() + ' calories');
+
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+console.log('Hamburger price with mayonnaise and sauce is: ' + hamburger.calculatePrice() + ' tugriks');
+console.log('Hamburger calories with mayonnaise and sauce: ' + hamburger.calculateCalories() + ' calories');
+
+console.log(hamburger);
